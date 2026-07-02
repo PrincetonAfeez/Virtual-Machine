43955ad7864dd9ea9a4ba3b15da8370f04305942
Princeton Afeez <princetonafeez@gmail.com>
2026-07-02 09:50:36 -0700

    Python stack-based virtual machine

---
+++ src/pvm/__init__.py
@@ -0,0 +1,61 @@
+"""Python stack-based virtual machine."""
+
+from .assembler import assemble, assemble_file
+from .bytecode import (
+    deserialize_program,
+    load_program,
+    save_program,
+    serialize_program,
+)
+from .disassembler import disassemble
+from .errors import (
+    AssemblerError,
+    BytecodeValidationError,
+    CallDepthError,
+    DivisionByZeroError,
+    InvalidCallError,
+    InvalidJumpError,
+    PVMError,
+    StackOverflowError,
+    StackUnderflowError,
+    StepLimitError,
+    TypeMismatchError,
+    UninitializedLocalError,
+    VMError,
+)
+from .format import format_value
+from .program import Function, Program
+from .validate import validate_program
+from .vm import VM, VMConfig
+
+__all__ = [
+    "AssemblerError",
+    "BytecodeValidationError",
+    "CallDepthError",
+    "DivisionByZeroError",
+    "Function",
+    "InvalidCallError",
+    "InvalidJumpError",
+    "PVMError",
+    "Program",
+    "StackOverflowError",
+    "StackUnderflowError",
+    "StepLimitError",
+    "TypeMismatchError",
+    "UninitializedLocalError",
+    "VM",
+    "VMConfig",
+    "VMError",
+    "assemble",
+    "assemble_file",
+    "deserialize_program",
+    "disassemble",
+    "format_value",
+    "load_program",
+    "save_program",
+    "serialize_program",
+    "validate_program",
+    "__version__",
+]
+
+__version__ = "1.0.4"
